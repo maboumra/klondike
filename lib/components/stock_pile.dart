e3ce58eb069815b52a578249a96a1d5a8f780ac2
Marcelo Abu <comercial@ijk.com.br>
2023-12-13 08:19:44 -0300

    complete project

--- lib/components/stock_pile.dart
+++ lib/components/stock_pile.dart
@@ -1,14 +1,14 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
-import 'package:flame/events.dart';
-import 'package:flutter/painting.dart';
-import 'package:klondike/components/pile.dart';
-import 'package:klondike/components/waste_pile.dart';
 
 import '../klondike_game.dart';
+import 'pile.dart';
 import 'card.dart';
+import 'waste_pile.dart';
 
 class StockPile extends PositionComponent
-    with TapCallbacks, HasGameReference<KlondikeGame>
+    with HasGameReference<KlondikeGame>
     implements Pile {
   StockPile({super.position}) : super(size: KlondikeGame.cardSize);
 
@@ -16,19 +16,39 @@ class StockPile extends PositionComponent
   /// list is at the bottom, the last card is on top.
   final List<Card> _cards = [];
 
+  //#region Pile API
+
+  @override
+  bool canMoveCard(Card card, MoveMethod method) => false;
+  // Can be moved by onTapUp callback (see below).
+
+  @override
+  bool canAcceptCard(Card card) => false;
+
+  @override
+  void removeCard(Card card, MoveMethod method) =>
+      throw StateError('cannot remove cards');
+
+  @override
+  // Card cannot be removed but could have been dragged out of place.
+  void returnCard(Card card) => card.priority = _cards.indexOf(card);
+
   @override
   void acquireCard(Card card) {
-    assert(!card.isFaceUp);
+    assert(card.isFaceDown);
+    card.pile = this;
     card.position = position;
     card.priority = _cards.length;
     _cards.add(card);
-    card.pile = this;
   }
 
-  @override
-  void onTapUp(TapUpEvent event) {
+  //#endregion
+
+  void handleTapUp(Card card) {
     final wastePile = parent!.firstChild<WastePile>()!;
     if (_cards.isEmpty) {
+      assert(card.isBaseCard, 'Stock Pile is empty, but no Base Card present');
+      card.position = position; // Force Base Card (back) into correct position.
       wastePile.removeAllCards().reversed.forEach((card) {
         card.flip();
         acquireCard(card);
@@ -37,12 +57,18 @@ class StockPile extends PositionComponent
       for (var i = 0; i < game.klondikeDraw; i++) {
         if (_cards.isNotEmpty) {
           final card = _cards.removeLast();
-          card.flip();
-          wastePile.acquireCard(card);
+          card.doMoveAndFlip(
+            wastePile.position,
+            whenDone: () {
+              wastePile.acquireCard(card);
+            },
+          );
         }
       }
     }
   }
+
+  //#region Rendering
 
   final _borderPaint = Paint()
     ..style = PaintingStyle.stroke
@@ -63,17 +89,5 @@ class StockPile extends PositionComponent
     );
   }
 
-  @override
-  bool canMoveCard(Card card) => false;
-
-  @override
-  bool canAcceptCard(Card card) => false;
-
-  @override
-  void removeCard(Card card) =>
-      throw StateError('cannot remove cards from here');
-
-  @override
-  void returnCard(Card card) =>
-      throw StateError('cannot remove cards from here');
+//#endregion
 }
--- lib/components/waste_pile.dart
+++ lib/components/waste_pile.dart
@@ -1,7 +1,7 @@
 import 'package:flame/components.dart';
-import 'package:klondike/components/pile.dart';
 
 import '../klondike_game.dart';
+import 'pile.dart';
 import 'card.dart';
 
 class WastePile extends PositionComponent
@@ -10,22 +10,51 @@ class WastePile extends PositionComponent
   WastePile({super.position}) : super(size: KlondikeGame.cardSize);
 
   final List<Card> _cards = [];
+  final Vector2 _fanOffset = Vector2(KlondikeGame.cardWidth * 0.2, 0);
+
+  //#region Pile API
+
+  @override
+  bool canMoveCard(Card card, MoveMethod method) =>
+      _cards.isNotEmpty && card == _cards.last; // Tap and drag are both OK.
+
+  @override
+  bool canAcceptCard(Card card) => false;
+
+  @override
+  void removeCard(Card card, MoveMethod method) {
+    assert(canMoveCard(card, method));
+    _cards.removeLast();
+    _fanOutTopCards();
+  }
+
+  @override
+  void returnCard(Card card) {
+    card.priority = _cards.indexOf(card);
+    _fanOutTopCards();
+  }
 
   @override
   void acquireCard(Card card) {
     assert(card.isFaceUp);
+    card.pile = this;
     card.position = position;
     card.priority = _cards.length;
     _cards.add(card);
     _fanOutTopCards();
-    card.pile = this;
   }
 
-  final Vector2 _fanOffset = Vector2(KlondikeGame.cardWidth * 0.2, 0);
+  //#endregion
+
+  List<Card> removeAllCards() {
+    final cards = _cards.toList();
+    _cards.clear();
+    return cards;
+  }
 
   void _fanOutTopCards() {
     if (game.klondikeDraw == 1) {
-      _cards.last.position = position;
+      // No fan-out in Klondike Draw 1.
       return;
     }
     final n = _cards.length;
@@ -38,30 +67,5 @@ class WastePile extends PositionComponent
       _cards[n - 2].position.add(_fanOffset);
       _cards[n - 1].position.addScaled(_fanOffset, 2);
     }
-  }
-
-  List<Card> removeAllCards() {
-    final cards = _cards.toList();
-    _cards.clear();
-    return cards;
-  }
-
-  @override
-  bool canMoveCard(Card card) => _cards.isNotEmpty && card == _cards.last;
-
-  @override
-  bool canAcceptCard(Card card) => false;
-
-  @override
-  void removeCard(Card card) {
-    assert(canMoveCard(card));
-    _cards.removeLast();
-    _fanOutTopCards();
-  }
-
-  @override
-  void returnCard(Card card) {
-    card.priority = _cards.indexOf(card);
-    _fanOutTopCards();
   }
 }
